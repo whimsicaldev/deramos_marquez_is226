1176a4a73ebb0a916286b7d66fe1431500b6561a
gerarrd <deramos.gerard@gmail.com>
2022-04-22 21:10:26 +0800

    updating validations

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,27 +10,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/user')]
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_user_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
-    public function show(User $user): Response
-    {
-        return $this->render('user/show.html.twig', [
-            'user' => $user,
-        ]);
-    }
-
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
     public function edit(UserPasswordHasherInterface $passwordHasher, Request $request, User $user, UserRepository $userRepository): Response
     {
@@ -45,9 +30,7 @@ class UserController extends AbstractController
             $existingUser = $userRepository->loadUserByIdentifier($user->getUsername());
 
             if($existingUser != null) {
-                $form
-                    ->get('username')
-                    ->addError(new FormError('Username is already in use.'));
+                $userForm->addError(new FormError('Username is already in use.'));
             } else {
                 $userRepository->add($user);
                 $toast = "Profile successfully updated.";
