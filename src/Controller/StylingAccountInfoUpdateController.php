dfd6ef8263c43d9ec0f6729914957040a1b23221
JC MARQUEZ <jcmarquez+tech@pm.me>
2022-04-27 15:06:55 +0800

    styling account info update

---
+++ src/Controller/StylingAccountInfoUpdateController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StylingAccountInfoUpdateController extends AbstractController
+{
+    #[Route('/accountinfoupdate', name: 'app_styling_account_info_update')]
+    public function index(): Response
+    {
+        return $this->render('styling_account_info_update/index.html.twig', [
+            'controller_name' => 'StylingAccountInfoUpdateController',
+        ]);
+    }
+}
