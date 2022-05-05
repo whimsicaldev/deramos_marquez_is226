33d838b714c2917ec26e9e18808b1981865ad823
JC MARQUEZ <jcmarquez+tech@pm.me>
2022-05-05 18:36:03 +0800

    styling admin update

---
+++ src/Controller/StylingAdminUpdateController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StylingAdminUpdateController extends AbstractController
+{
+    #[Route('/styling/admin/update', name: 'app_styling_admin_update')]
+    public function index(): Response
+    {
+        return $this->render('styling_admin_update/index.html.twig', [
+            'controller_name' => 'StylingAdminUpdateController',
+        ]);
+    }
+}
