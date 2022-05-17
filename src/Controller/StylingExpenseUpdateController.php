9131093d2a9daaacbcfb20c9c51f98bf1e5915ae
JC MARQUEZ <jcmarquez+tech@pm.me>
2022-05-17 16:44:05 +0800

    styling update expense

--- src/Controller/StylingExpenseUpdateController.php
+++ src/Controller/StylingExpenseUpdateController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class StylingExpenseUpdateController extends AbstractController
 {
-    #[Route('/styling/expense/update', name: 'app_styling_expense_update')]
+    #[Route('/expenseupdatestyle', name: 'app_styling_expense_update')]
     public function index(): Response
     {
         return $this->render('styling_expense_update/index.html.twig', [
