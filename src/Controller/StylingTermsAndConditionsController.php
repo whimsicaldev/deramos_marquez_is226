1265306d3d0354ebba0b74a2069547f7fcc8cb52
JC MARQUEZ <jcmarquez+tech@pm.me>
2022-05-17 16:48:29 +0800

    styling terms and conditions

---
+++ src/Controller/StylingTermsAndConditionsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StylingTermsAndConditionsController extends AbstractController
+{
+    #[Route('/styling/terms/and/conditions', name: 'app_styling_terms_and_conditions')]
+    public function index(): Response
+    {
+        return $this->render('styling_terms_and_conditions/index.html.twig', [
+            'controller_name' => 'StylingTermsAndConditionsController',
+        ]);
+    }
+}
