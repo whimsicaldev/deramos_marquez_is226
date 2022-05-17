92dddc39f29ec86602babd10e9707189d38a25ee
JC MARQUEZ <jcmarquez+tech@pm.me>
2022-05-17 16:39:24 +0800

    styling expenses

---
+++ src/Controller/StylingExpenseUpdateController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StylingExpenseUpdateController extends AbstractController
+{
+    #[Route('/styling/expense/update', name: 'app_styling_expense_update')]
+    public function index(): Response
+    {
+        return $this->render('styling_expense_update/index.html.twig', [
+            'controller_name' => 'StylingExpenseUpdateController',
+        ]);
+    }
+}
